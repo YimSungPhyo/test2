878c54a7192c3569dd688f19d5d341592918d765
YimJunHyuk <sungs6031@naver.com>
2025-10-29 14:01:56 +0000

    ff

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,49 @@
+package org.zerock.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.SampleVO;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+@RestController //rest 방식의 컨트롤러 입니다.
+public class SampleController {
+
+	
+	//@GetMapping("/hello")
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World22";
+	}
+	
+	
+	//@GetMapping("/sample")
+	@RequestMapping("/sample")
+	public SampleVO makeSample() {
+		
+		SampleVO vo = new SampleVO();
+		vo.setVal1("V1");
+		vo.setVal2("V2");
+		vo.setVal3(StringUtils.replace("돈이_엄나", "_", "") );
+		
+		System.out.print(vo);
+		
+		return vo;
+	}
+	
+	
+	@GetMapping("/sampleString")
+	public String makeSampleString() {
+		
+		SampleVO vo = new SampleVO();
+		vo.setVal1("V1");
+		vo.setVal2("V2");
+		vo.setVal3("V3");
+		
+		return vo.toString();
+	}
+}
+
